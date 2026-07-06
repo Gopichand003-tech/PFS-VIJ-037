246367a0452844a41adf9196373c1e5a9f1343cc
Gopichand Chennapalli <gopichandchennapalli@gmail.com>
2026-07-06 12:53:35 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,154 @@
+#conditions
+#if - condition by using comparision opeartors
+#<,>,<=,>=,!=,==
+
+'''a=10
+b=20
+if(a<b:
+   print("true")'''
+
+'''a=10
+b=20
+if a>b:
+    print("true")'''
+
+'''a=5
+b=7
+if a<=b
+   print("true")'''
+
+'''a=12
+b=15
+if a>=b:
+    print("true")'''
+
+'''a=30
+b=40
+if a!=b
+   print("true")'''
+
+'''a="python"
+if a=="python":
+    print("match")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")'''
+
+#if - condition by using logical operators
+#and,or,not
+'''a=3
+b=6
+if a<b and b>a:
+    print("true")'''
+
+'''a=4
+b=7
+if a<=b and b>=a:
+    print("true")'''
+
+'''a=9
+b=12
+if a!=b and a==b:
+    print("true")'''
+
+'''a=2
+b=4
+if a<b or b>a:
+    print("true")'''
+
+'''a=14
+b=16
+if a<=b or b>=a:
+    print("true")'''
+
+'''a=3
+b=6
+if a!=b or a==b:
+    print("true")'''
+
+'''a=5
+b=7
+if not a<b:
+    print("true")'''
+
+'''a=3
+b=6
+if not a<b and b>a:
+    print("true")'''
+
+#if-condition by using identify operators
+#is,is not
+'''a=4
+if type(a) is int:
+    print("is is int")'''
+
+'''a = 4
+if type(a) is not int:
+    print("is is int")'''
+
+
+'''a=5
+if type(a) is not bool:
+    print("is is bool")'''
+
+'''a=5
+if type(a) is bool:
+    print("is is bool")'''
+    
+'''a=5
+if type(a) is  str:
+    print("is is str")'''
+
+'''a=5
+if type(a) is not str:
+    print("is is str")'''
+'''a=5
+if type(a) is  complex:
+    print("is is complex")'''
+'''a=5
+if type(a) is  not complex:
+    print("is is complex")'''
+
+#if-condition by using membership operators
+'''a=2,3,4,5,6,7,8,9
+if 8 in a:
+    print("true")'''
+
+
+'''a=2,3,4,5,6,7,8
+if 20 not in a:
+    print("true")'''
+
+'''a=int(input("a value"))
+if 30 in a:
+    print("true")'''#error
+
+'''a=2,3,4,5,6,7,8,9,10
+b=int(input("value"))
+if b in a:
+    print("true")'''
+
+#if-else condition by using membership operators
+'''a=4
+b=8
+if a<b:
+    print("less")
+else:
+    print("false")'''
+
+
+'''a=4
+b=8
+if a>b:
+    print("less")
+else:
+    print("false")'''
+
+    
+        
+
+    
+   
+   
